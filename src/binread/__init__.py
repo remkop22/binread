94c2935c65871b1636eec929de5968f92023b7a6
Remo Pas <remopas@hotmail.nl>
2022-05-18 23:35:48 +0200

    added faster bytes type to init

--- src/binread/__init__.py
+++ src/binread/__init__.py
@@ -1,6 +1,6 @@
 
 from .format import Format, format
-from .types import Array, String, Bool, Char
+from .types import Array, String, Bool, Char, Bytes
 from .types import I8, I16, I32, I64
 from .types import U8, U16, U32, U64
 from .types import F16, F32, F64
